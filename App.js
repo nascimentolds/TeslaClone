a823fd2c420fc8d373022973e1e0124196cf0bf4
nascimentolds <47907106+nascimentolds@users.noreply.github.com>
2021-05-18 18:02:24 -0300

    Header and FlatList

--- App.js
+++ App.js
@@ -3,10 +3,12 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import CarsList from './components/CarsList'
+import Header from './components/Header'
 
 export default function App() {
   return (
-    <View style={styles.container}>      
+    <View style={styles.container}> 
+      <Header />     
       <CarsList />
       <StatusBar style="auto"/>
     </View>
